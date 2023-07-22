8f13fef9a98e49eeb808a4bc7a0ffc2debc41754
John Trochez <johndtf@hotmail.com>
2023-07-22 17:38:00 -0500

    menus modal estilo cristal y creación de opción ordenes

---
+++ js/ordenes.js
@@ -0,0 +1,20 @@
+// Obtener el elemento de la cabecera de la columna
+var checkAll = document.getElementById("checkAll");
+
+// Escuchar el evento click del elemento de la cabecera de la columna
+checkAll.addEventListener("click", function () {
+  // Obtener todos los elementos checkbox de la columna
+  var checkboxes = document.querySelectorAll("td input[type=checkbox]");
+
+  // Si el elemento de la cabecera de la columna está chequeado, chequear todos los elementos checkbox de la columna
+  if (this.checked) {
+    for (var i = 0; i < checkboxes.length; i++) {
+      checkboxes[i].checked = true;
+    }
+  } else {
+    // Si el elemento de la cabecera de la columna no está chequeado, deschequear todos los elementos checkbox de la columna
+    for (var i = 0; i < checkboxes.length; i++) {
+      checkboxes[i].checked = false;
+    }
+  }
+});
