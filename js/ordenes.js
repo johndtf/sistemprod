64572f41f24377f704ca127e585df5abda9816ec
John Trochez <johndtf@hotmail.com>
2025-06-15 12:29:36 -0700

    agregar listados de marca dimension y disenio en el formulario de ordenes

--- js/ordenes.js
+++ js/ordenes.js
@@ -1,7 +1,11 @@
-/* --------Tabla - Check de la cabecera ------------------------------ */
-// Obtener el elemento de la cabecera de la columna
-var checkAll = document.getElementById("checkAll");
+const checkAll = document.getElementById("checkAll");
+const marcaField = document.getElementById("marca");
+const dimensionField = document.getElementById("dimension");
+const disenoField = document.getElementById("diseno");
 
+import apiUrl from "./config.js";
+
+/* --------Tabla - funcionamiento del Check de la cabecera ------------------------------ */
 // Escuchar el evento click del elemento de la cabecera de la columna
 checkAll.addEventListener("click", function () {
   // Obtener todos los elementos checkbox de la columna
@@ -20,3 +24,77 @@ checkAll.addEventListener("click", function () {
   }
 });
 /* -------------Fin Tabla - check de la cabecera----------------- */
+
+/* =============Procedimientos después de cargar el formulario ============= */
+
+document.addEventListener("DOMContentLoaded", () => {
+  /* --------------------------Carga de marcas-------------------------- */
+  // Realizar una solicitud al servidor para obtener la lista de marcas
+  const token = localStorage.getItem("myTokenName");
+
+  fetch(`${apiUrl}/api/brands/brandslist`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Llenar dinámicamente el select con el listado de marcas
+
+      data.forEach((marca) => {
+        const option = document.createElement("option");
+        option.value = marca.id_marca; // Asigna el id de la marca
+        option.text = marca.marca; // Asigna el nombre del perfil
+        marcaField.appendChild(option);
+      });
+    })
+    .catch((error) => console.error("Error al cargar marcas", error));
+
+  /* --------------------------Carga de Dimensiones-------------------------- */
+  // Realizar una solicitud al servidor para obtener la lista de dimensiones
+
+  fetch(`${apiUrl}/api/dimensions/dimensionslist`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Llenar dinámicamente el select con el listado
+
+      data.forEach((dimension) => {
+        const option = document.createElement("option");
+        option.value = dimension.id_dimension;
+        option.text = dimension.dimension;
+        dimensionField.appendChild(option);
+      });
+    })
+    .catch((error) => console.error("Error al cargar dimensiones", error));
+
+  /* --------------------------Carga de Diseños-------------------------- */
+  // Realizar una solicitud al servidor para obtener la lista de diseños
+
+  fetch(`${apiUrl}/api/treads/treadlist`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Llenar dinámicamente el select con el listado
+
+      data.forEach((banda) => {
+        const option = document.createElement("option");
+        option.value = banda.id_banda;
+        option.text = banda.banda;
+        disenoField.appendChild(option);
+      });
+    })
+    .catch((error) => console.error("Error al cargar dimensiones", error));
+});
